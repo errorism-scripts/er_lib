1e7de4a333746953b762c2fca0507a48ae407f1b
bsrypt <bsry.nitipoom@gmail.com>
2025-05-12 15:37:54 +0700

    chore: change to er_lib
    The purpose of this fork is to add new feature from the original author
    overextended that we mostly use in our errorism-script and respect the
    license

--- imports/require/shared.lua
+++ imports/require/shared.lua
@@ -10,42 +10,36 @@ local loaded = {}
 local _require = require
 
 package = {
-    path = './?.lua;./?/init.lua',
-    preload = {},
-    loaded = setmetatable({}, {
-        __index = loaded,
-        __newindex = noop,
-        __metatable = false,
-    })
+  path = './?.lua;./?/init.lua',
+  preload = {},
+  loaded = setmetatable({}, {
+    __index = loaded,
+    __newindex = noop,
+    __metatable = false,
+  }),
 }
 
 ---@param modName string
 ---@return string
 ---@return string
 local function getModuleInfo(modName)
-    local resource = modName:match('^@(.-)/.+') --[[@as string?]]
+  local resource = modName:match '^@(.-)/.+' --[[@as string?]]
 
-    if resource then
-        return resource, modName:sub(#resource + 3)
-    end
+  if resource then return resource, modName:sub(#resource + 3) end
 
-    local idx = 4 -- call stack depth (kept slightly lower than expected depth "just in case")
+  local idx = 4 -- call stack depth (kept slightly lower than expected depth "just in case")
 
-    while true do
-        local src = debug.getinfo(idx, 'S')?.source
+  while true do
+    local src = debug.getinfo(idx, 'S')?.source
 
-        if not src then
-            return cache.resource, modName
-        end
+    if not src then return cache.resource, modName end
 
-        resource = src:match('^@@([^/]+)/.+')
+    resource = src:match '^@@([^/]+)/.+'
 
-        if resource and not src:find('^@@ox_lib/imports/require') then
-            return resource, modName
-        end
+    if resource and not src:find '^@@er_lib/imports/require' then return resource, modName end
 
-        idx += 1
-    end
+    idx += 1
+  end
 end
 
 local tempData = {}
@@ -56,23 +50,23 @@ local tempData = {}
 ---@return string? errmsg
 ---@diagnostic disable-next-line: duplicate-set-field
 function package.searchpath(name, path)
-    local resource, modName = getModuleInfo(name:gsub('%.', '/'))
-    local tried = {}
+  local resource, modName = getModuleInfo(name:gsub('%.', '/'))
+  local tried = {}
 
-    for template in path:gmatch('[^;]+') do
-        local fileName = template:gsub('^%./', ''):gsub('?', modName:gsub('%.', '/') or modName)
-        local file = LoadResourceFile(resource, fileName)
+  for template in path:gmatch '[^;]+' do
+    local fileName = template:gsub('^%./', ''):gsub('?', modName:gsub('%.', '/') or modName)
+    local file = LoadResourceFile(resource, fileName)
 
-        if file then
-            tempData[1] = file
-            tempData[2] = resource
-            return fileName
-        end
-
-        tried[#tried + 1] = ("no file '@%s/%s'"):format(resource, fileName)
+    if file then
+      tempData[1] = file
+      tempData[2] = resource
+      return fileName
     end
 
-    return nil, table.concat(tried, "\n\t")
+    tried[#tried + 1] = ('no file \'@%s/%s\''):format(resource, fileName)
+  end
+
+  return nil, table.concat(tried, '\n\t')
 end
 
 ---Attempts to load a module at the given path relative to the resource root directory.\
@@ -80,17 +74,17 @@ end
 ---@param modName string
 ---@param env? table
 local function loadModule(modName, env)
-    local fileName, err = package.searchpath(modName, package.path)
+  local fileName, err = package.searchpath(modName, package.path)
 
-    if fileName then
-        local file = tempData[1]
-        local resource = tempData[2]
+  if fileName then
+    local file = tempData[1]
+    local resource = tempData[2]
 
-        table.wipe(tempData)
-        return assert(load(file, ('@@%s/%s'):format(resource, fileName), 't', env or _ENV))
-    end
+    table.wipe(tempData)
+    return assert(load(file, ('@@%s/%s'):format(resource, fileName), 't', env or _ENV))
+  end
 
-    return nil, err or 'unknown error'
+  return nil, err or 'unknown error'
 end
 
 ---@alias PackageSearcher
@@ -99,21 +93,21 @@ end
 
 ---@type PackageSearcher[]
 package.searchers = {
-    function(modName)
-        local ok, result = pcall(_require, modName)
+  function(modName)
+    local ok, result = pcall(_require, modName)
 
-        if ok then return result end
+    if ok then return result end
 
-        return ok, result
-    end,
-    function(modName)
-        if package.preload[modName] ~= nil then
-            return package.preload[modName]
-        end
+    return ok, result
+  end,
+  function(modName)
+    if package.preload[modName] ~= nil then return package.preload[modName] end
 
-        return nil, ("no field package.preload['%s']"):format(modName)
-    end,
-    function(modName) return loadModule(modName) end,
+    return nil, ('no field package.preload[\'%s\']'):format(modName)
+  end,
+  function(modName)
+    return loadModule(modName)
+  end,
 }
 
 ---@param filePath string
@@ -121,33 +115,27 @@ package.searchers = {
 ---@return unknown
 ---Loads and runs a Lua file at the given path. Unlike require, the chunk is not cached for future use.
 function lib.load(filePath, env)
-    if type(filePath) ~= 'string' then
-        error(("file path must be a string (received '%s')"):format(filePath), 2)
-    end
+  if type(filePath) ~= 'string' then error(('file path must be a string (received \'%s\')'):format(filePath), 2) end
 
-    local result, err = loadModule(filePath, env)
+  local result, err = loadModule(filePath, env)
 
-    if result then return result() end
+  if result then return result() end
 
-    error(("file '%s' not found\n\t%s"):format(filePath, err))
+  error(('file \'%s\' not found\n\t%s'):format(filePath, err))
 end
 
 ---@param filePath string
 ---@return table
 ---Loads and decodes a json file at the given path.
 function lib.loadJson(filePath)
-    if type(filePath) ~= 'string' then
-        error(("file path must be a string (received '%s')"):format(filePath), 2)
-    end
+  if type(filePath) ~= 'string' then error(('file path must be a string (received \'%s\')'):format(filePath), 2) end
 
-    local resourceSrc, modPath = getModuleInfo(filePath:gsub('%.', '/'))
-    local resourceFile = LoadResourceFile(resourceSrc, ('%s.json'):format(modPath))
+  local resourceSrc, modPath = getModuleInfo(filePath:gsub('%.', '/'))
+  local resourceFile = LoadResourceFile(resourceSrc, ('%s.json'):format(modPath))
 
-    if resourceFile then
-        return json.decode(resourceFile)
-    end
+  if resourceFile then return json.decode(resourceFile) end
 
-    error(("json file '%s' not found\n\tno file '@%s/%s.json'"):format(filePath, resourceSrc, modPath))
+  error(('json file \'%s\' not found\n\tno file \'@%s/%s.json\''):format(filePath, resourceSrc, modPath))
 end
 
 ---Loads the given module, returns any value returned by the seacher (`true` when `nil`).\
@@ -155,36 +143,34 @@ end
 ---@param modName string
 ---@return unknown
 function lib.require(modName)
-    if type(modName) ~= 'string' then
-        error(("module name must be a string (received '%s')"):format(modName), 3)
+  if type(modName) ~= 'string' then error(('module name must be a string (received \'%s\')'):format(modName), 3) end
+
+  local module = loaded[modName]
+
+  if module == '__loading' then
+    error(('^1circular-dependency occurred when loading module \'%s\'^0'):format(modName), 2)
+  end
+
+  if module ~= nil then return module end
+
+  loaded[modName] = '__loading'
+
+  local err = {}
+
+  for i = 1, #package.searchers do
+    local result, errMsg = package.searchers[i](modName)
+
+    if result then
+      if type(result) == 'function' then result = result() end
+      loaded[modName] = result or result == nil
+
+      return loaded[modName]
     end
 
-    local module = loaded[modName]
+    err[#err + 1] = errMsg
+  end
 
-    if module == '__loading' then
-        error(("^1circular-dependency occurred when loading module '%s'^0"):format(modName), 2)
-    end
-
-    if module ~= nil then return module end
-
-    loaded[modName] = '__loading'
-
-    local err = {}
-
-    for i = 1, #package.searchers do
-        local result, errMsg = package.searchers[i](modName)
-
-        if result then
-            if type(result) == 'function' then result = result() end
-            loaded[modName] = result or result == nil
-
-            return loaded[modName]
-        end
-
-        err[#err + 1] = errMsg
-    end
-
-    error(("%s"):format(table.concat(err, "\n\t")))
+  error(('%s'):format(table.concat(err, '\n\t')))
 end
 
 return lib.require

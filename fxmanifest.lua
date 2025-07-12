f9cea34ce0b94e4b75604dc432d86f594dc8b408
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-12 07:02:46 +0000

    chore: bump manifest version to v1.5.7

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 name 'er_lib'
 author 'Overextended, Fork by Errorism'
-version '1.5.6'
+version '1.5.7'
 license 'LGPL-3.0-or-later'
 repository 'https://github.com/errorism-scripts/er_lib'
 description 'A library of shared functions to utilise in other resources.'
